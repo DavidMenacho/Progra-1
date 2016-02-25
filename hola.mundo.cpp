f408c1008be5d882bb50384dcbe1bbf0e8db62dd
DavidMenacho <davidmenacho@icloud.com>
2016-02-25 10:14:14 -0400

    Added files via upload

---
+++ hola.mundo.cpp
@@ -0,0 +1,16 @@
+// hola mundo.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdafx.h"
+#include <iostream>
+#include "comio.h"
+
+using namespace std;
+
+{
+	cout<<"Hola Mundo";
+	getch();
+
+}
+
